0db6c085573d9a0269f16782e1bbab92eb35ccde
ASHUTOSH SINGH <155178478+ashu-94@users.noreply.github.com>
2026-09-16 17:05:03 +0530

    Add migrations project files

---
+++ migrations/env.py
@@ -0,0 +1,9 @@
+from alembic import context
+
+from backend import models  # noqa: F401
+from backend.db import Base, engine
+
+with engine.connect() as connection:
+    context.configure(connection=connection, target_metadata=Base.metadata, compare_type=True)
+    with context.begin_transaction():
+        context.run_migrations()
